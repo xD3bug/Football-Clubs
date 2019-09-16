76bcff0b0f913f63f3ee28fc5b72644a04f3c4bd
xD3bug <aslan.kunashev@gmail.com>
2019-09-16 19:18:08 +0300

    Initial Commit

---
+++ Football Clubs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Football Clubs
+//
+//  Created by Аслан on 16/09/2019.
+//  Copyright © 2019 Doka.fun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
